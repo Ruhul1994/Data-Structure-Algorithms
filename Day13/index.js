a1f8686c554353e4df537c2d2e5fc5ded00a6edc
Ruhul1994 <k.m.ruhulkuddus@gmail.com>
2025-02-18 00:21:56 +0600

    refactor: comment out unused code and add new array utility functions

--- Day13/index.js
+++ Day13/index.js
@@ -15,12 +15,61 @@
 // console.log(result);
 
 
-myArray = [1,2,3,4,5,6,7,8];
+// myArray = [1,2,3,4,5,6,7,8];
 
-for(let i = 0; i<= myArray.length; i++){
-    console.log("Length is: " + myArray.length);
-    console.log(myArray[i]);
-    if(myArray[i] == undefined){
-        return console.log("Array is not in sequence");
-    }
-}
+// for(let i = 0; i<= myArray.length; i++){
+//     console.log("Length is: " + myArray.length);
+//     console.log(myArray[i]);
+//     if(myArray[i] == undefined){
+//         return console.log("Array is not in sequence");
+//     }
+// }
+
+
+
+// 2️⃣ Check if an array contains duplicates.
+
+const myArray = [1, 2, 3, 4, 5, 6, 7,7,8, 8, 9, -10];
+// Find the minimum number in an array.
+// function findMin(arr){
+//     let min = arr[0];
+//     for(let i=0; i<arr.length; i++){
+//         if(arr[i] < min ){
+//             min = arr[i];
+//         }
+//     }
+//     return min;
+// }
+
+// console.log(findMin(myArray));
+
+// 3️⃣ Remove all even numbers from an array.
+
+// function removeEven(arr){
+//     let result = [];
+//     for(let i=0; i<arr.length; i++){
+//         if(arr[i] % 2 !== 0){
+//             result.push(arr[i]);
+//         }
+//     }
+
+//     return result;
+// }
+
+// console.log(removeEven(myArray));
+
+// Check if an array contains duplicates.
+
+
+// function checkDuplicates(arr){
+//     let result = [];
+//     for(let i=0; i<arr.length; i++){
+//         if(result.includes(arr[i])){
+//             return true;
+//         }
+//         result.push(arr[i]);   
+//     }
+//     return false;
+// }
+
+// console.log(checkDuplicates(myArray));
