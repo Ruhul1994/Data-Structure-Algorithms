ac299e357f8517920420153d3f2a8ed7f4d938de
Ruhul1994 <k.m.ruhulkuddus@gmail.com>
2025-04-21 10:58:40 +0600

    refactor comments: update and format problem statements and examples in index.js

--- Day1/index.js
+++ Day1/index.js
@@ -5,15 +5,36 @@
 // }
 // min5log(4);
 
-function uniqName(array){
-let uniqName =[];
-for(let i=0; i<array.length; i++){
-    let eachName = array[i];
-    if(!uniqName.includes(eachName)){
-        uniqName.push(eachName);
-    }
-}
-return uniqName;
-}
-let nameArray =[ "suomi","Rased","mina","mina","tomas", "suomi","Rased"];
-console.log(uniqName(nameArray));
+// function uniqName(array){
+// let uniqName =[];
+// for(let i=0; i<array.length; i++){
+//     let eachName = array[i];
+//     if(!uniqName.includes(eachName)){
+//         uniqName.push(eachName);
+//     }
+// }
+// return uniqName;
+// }
+// let nameArray =[ "suomi","Rased","mina","mina","tomas", "suomi","Rased"];
+// console.log(uniqName(nameArray));
+
+
+// Problem 1: Two Sum
+
+//     Topic: Arrays, Hash Maps
+
+//     Math/Logic Used: Basic Arithmetic, Hashing
+
+//     Problem: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// Input: nums = [2, 7, 11, 15], target = 9
+// Output: [0, 1]
+
+// 
+
+Problem 2: Reverse a String
+
+    Topic: Strings, Two-pointer
+
+    Math/Logic Used: Index Manipulation
+
+    Problem: Reverse the characters in a string array in-place.
